9702e7e463ed8050765ab28e536cc440c3a46283
fbesoainp <chachibesoain1@gmail.com>
2023-05-07 20:34:32 -0400

    🔧 Basic Config

---
+++ server/config/database.js
@@ -0,0 +1,27 @@
+require('dotenv').config();
+
+const config = {
+  development: {
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB,
+    host: process.env.POSTGRES_HOST || '127.0.0.1',
+    dialect: process.env.DB_DIALECT || 'postgres',
+  },
+  test: {
+    username: 'root',
+    password: null,
+    database: 'database_test',
+    host: '127.0.0.1',
+    dialect: 'postgres',
+  },
+  production: {
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB,
+    host: process.env.POSTGRES_HOST,
+    dialect: process.env.DB_DIALECT || 'postgres',
+  },
+};
+
+module.exports = config;
