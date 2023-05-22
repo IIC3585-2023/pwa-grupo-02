62906e2f03bdbca125e8024e0bb33615edff33cc
fbesoainp <chachibesoain1@gmail.com>
2023-05-21 23:31:21 -0400

    🚀 Auhtenticate on startup

--- server/index.js
+++ server/index.js
@@ -1,6 +1,17 @@
 /* eslint-disable no-console */
 const app = require('./app');
+const orm = require('./models/index.js');
 
-app.listen(3000, () => {
-  console.log('Server listening on port 3000');
-});
+(async () => {
+  orm.sequelize.authenticate()
+    .then(() => {
+      console.log('Connection has been established successfully.');
+    })
+    .catch((error) => {
+      console.error('Unable to connect to the database:', error);
+    });
+
+  app.listen(3000, () => {
+    console.log('Server listening on port 3000');
+  });
+})();
